7eedcef13d0baa201d752afbb4cfed895afce7cb
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-03 12:57:09 +0100

    all routes working, no crsf nor cors

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-# import requestModel
-# import dbconnect
+
 
 app = Flask(__name__)
+
 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:StrongAdminP@ssw0rd@localhost:5432/nin"
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 
 
 
-class IssuedDateModel(db.Model):
+class RegisteredData(db.Model):
     __tablename__ = "nin_slips"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -24,45 +24,82 @@ class IssuedDateModel(db.Model):
     gender = db.Column(db.String(1))
     issued_date = db.Column(db.Date)
     age_category = db.Column(db.String(6))
-    
-    # def __init__(self, id, tracking_id, nin, first_name, last_name, middle_name, gender, issued_date, age_category):
-    #     self.tracking_id = tracking_id
-    #     self.nin = nin
-    #     self.first_name = first_name
-    #     self.last_name = last_name
-    #     self.middle_name = middle_name
-    #     self.gender = gender
-    #     self.issued_date = issued_date
-    #     self.age_category = age_category
 
-    # def __repr__(self):
-    #     return f"(self.name)"
 
-# @app.route('/filtered_query/<nin_val>', methods=['GET'])
-@app.route('/filtered_query/<last_name>', methods=['GET'])
-def issued_date_request(last_name):
-    if request.method == 'GET':
-        # nin_data = IssuedDateModel.query.all()
-        # nin_val
-        # nin_val = 66005354139
-        # date_val = 14-1-2020
-        query_filter = IssuedDateModel.query.filter_by(nin='last_name').all()
-        # query_filter = IssuedDateModel.query.filter_by(nin=date_val).all()
-        # query_filter = IssuedDateModel.query.filter_by(nin=nin_val).all_or_404(description='There is no data with {}'.format(nin))
-        # query_filter = IssuedDateModel.query.filter_by(nin=nin_val).all()
-        # query_filter = IssuedDateModel.query.filter_by(nin='66005354139', issued_date='14-01-2020').all()
-        results = [
-            {
-                "nin": nin.nin,
-                "first_name": nin.first_name,
-                "last_name": nin.last_name,
-                "middle_name": nin.middle_name,
-                "issued_date": nin.issued_date
-            }
-            for nin in query_filter
-        ]    
 
-    JSONObjects = {"count": len(results), "query_filter": results}
+@app.route('/restricted_raw', methods=['GET'])
+def raw_request():
+    # if request.method == 'GET':
+    query_term = RegisteredData.query.all()
+    results = [
+        {
+            "nin": table_data.nin,
+            "first_name": table_data.first_name,
+            "last_name": table_data.last_name,
+            "middle_name": table_data.middle_name,
+            "issued_date": table_data.issued_date
+        }
+        for table_data in query_term
+    ]    
+
+    JSONObjects = {"count": len(results), "query_term": results}
+    return jsonify(JSONObjects), 200
+
+
+
+@app.route('/filter_by_nin/<nin>', methods=['GET'])
+def nin_request(nin):
+    # if request.method == 'GET':
+    query_term = RegisteredData.query.filter_by(nin=nin).all()
+    results = [
+        {
+            "first_name": table_data.first_name,
+            "last_name": table_data.last_name,
+            "middle_name": table_data.middle_name,
+            "issued_date": table_data.issued_date
+        }
+        for table_data in query_term
+    ]    
+
+    JSONObjects = {"count": len(results), "query_term": results}
+    return jsonify(JSONObjects), 200
+
+
+
+@app.route('/filter_by_last_name/<last_name>', methods=['GET'])
+def last_name_request(last_name):
+    # if request.method == 'GET':
+    query_term = RegisteredData.query.filter_by(last_name=last_name).all()
+    results = [
+        {
+            "nin": table_data.nin,
+            "first_name": table_data.first_name,
+            "middle_name": table_data.middle_name,
+            "issued_date": table_data.issued_date
+        }
+        for table_data in query_term
+    ]    
+
+    JSONObjects = {"count": len(results), "query_term": results}
+    return jsonify(JSONObjects)
+
+
+
+@app.route('/filter_by_date/<issued_date>', methods=['GET'])
+def issued_date_request(issued_date):
+    # if request.method == 'GET':
+    query_term = RegisteredData.query.filter_by(issued_date=issued_date).all()
+    results = [
+        {
+            "nin": table_data.nin,
+            "first_name": table_data.first_name,
+            "last_name": table_data.last_name,
+            "middle_name": table_data.middle_name
+        }
+        for table_data in query_term
+    ]    
+
+    JSONObjects = {"count": len(results), "query_term": results}
     return jsonify(JSONObjects)
 
 
